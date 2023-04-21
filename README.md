7859a0e5c6dbca6b7db7b1d01cc0b9dec55bc426
nvaccina <niccolovac@gmail.com>
2023-04-21 15:41:08 +0200

    Create le mail e stampate

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.<br
 **Bonus**<br>
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 ## Steps
+- Creare l'API che mi genera una mail random e pushare la mail nell'array mailList;
+- Nel mounted fare un ciclo for che mi ripeta la funzione getApi() per 10 volte, in modo da avere 10 mail random;
+- Stampare le mail random in una lista;
 
 
--- js/script.js
+++ js/script.js
@@ -3,9 +3,38 @@ const {createApp} = Vue;
 createApp({
 
   data(){
+
     return{
+      apiUrl: 'https://flynn.boolean.careers/exercises/api/random/mail?',
+
+      mailList: [],
+      mail:''
+
+
+    }
+  },
+
+  methods:{
+    getApi(){
+
+      console.log('PARTITO AXIOS');
+
+      axios.get(this.apiUrl)
+
+      .then(result =>{
+        console.log('result.data', result.data.response);
+        this.mail = result.data.response
+        this.mailList.push(this.mail)
+      })     
       
+    }
+  },
+
+  mounted(){
+    for (i = 0; i < 10; i++) {
+      this.getApi();
     }
   }
 
 }).mount('#app')
+
